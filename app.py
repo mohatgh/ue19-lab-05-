c9a76103bbd9d894b457fc3dc7863c43360f1b9d
mohamed taghezouit <m.taghezouit@student.helmo.be>
2024-11-25 12:19:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import requests
+
+def fetch_joke():
+    url = "https://v2.jokeapi.dev/joke/Any"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        joke_data = response.json()
+        if joke_data.get("type") == "single":
+            return joke_data.get("joke")
+        else:
+            return f"{joke_data.get('setup')} - {joke_data.get('delivery')}"
+    else:
+        return "Failed to retrieve a joke"
+
+if __name__ == "__main__":
+    print("Fetching a random joke from JokeAPI...\n")
+    print(fetch_joke())
 
